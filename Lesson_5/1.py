cfe41d3bf67278e137806fee9b24fc7f201d9740
Denis <Denis.Gorshkov76@gmail.com>
2019-03-22 20:46:59 +0300

    Выполнил все задани дз №5

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,54 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+from collections import namedtuple
+import cProfile
+
+
+class CompanyInfo:
+    company_list = namedtuple('Company', 'company_name company_profit sum_profit')
+    input_company = company_list(
+        company_name=[],
+        company_profit=[],
+        sum_profit=[],
+    )
+
+    more_middle = 0
+    under_middle = 0
+    middle_profit = 0
+
+    def __init__(self, quantity):
+        self.quantity = int(quantity)
+
+    def company(self):
+        for i in range(self.quantity):
+            self.input_company.company_name.append(input('Введите название предприятия: '))
+            list_profit = list(map(float, input('Введите прибыль за каждый квартал через пробел: ').split(' ')))
+            self.input_company.company_profit.append(list_profit)
+            self.input_company.sum_profit.append(sum(self.input_company.company_profit[i]))
+
+    def calc_middle_profit(self):
+        self.company()
+        self.middle_profit = sum(self.input_company.sum_profit) / quantity
+        self.more_middle = list(filter(lambda x: x > self.middle_profit, self.input_company.sum_profit))
+        self.under_middle = list(filter(lambda x: x < self.middle_profit, self.input_company.sum_profit))
+
+    def print_company_more_middle(self):
+        self.calc_middle_profit()
+        list_company = [self.input_company.company_name[self.input_company.sum_profit.index(i)]
+                        for i in self.more_middle]
+        return print(f'Название предприятия(й) прибыль которых выше среднего( {self.middle_profit}) : '
+                     f'{", ".join(list_company)}')
+
+    def print_company_under_middle(self):
+        list_company = [self.input_company.company_name[self.input_company.sum_profit.index(i)]
+                        for i in self.under_middle]
+        return print(f'Название предприятия(й) прибыль которых ниже среднего( {self.middle_profit}) : '
+                     f'{", ".join(list_company)}')
+
+
+quantity = int(input('Введите количество предприятий: '))
+my_company_list = CompanyInfo(quantity)
+
+cProfile.run('my_company_list.print_company_more_middle()')
+cProfile.run('my_company_list.print_company_under_middle()')
