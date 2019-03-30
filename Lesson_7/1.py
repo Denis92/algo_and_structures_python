b40ec526dc706a16571666b16cecbcea18895f35
Denis <Denis.Gorshkov76@gmail.com>
2019-03-30 11:35:15 +0300

    Выполнил все задани дз №7

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -3,4 +3,63 @@
 заданный случайными числами на промежутке [-100; 100). Выведите на экран
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
+В результате работы над этим заданием, был реализована своя функция для сортировки пузырёк(изнаально я не подсматривал
+в код, который писал преподаватель) В двльнейшем я сравнил две реализации в итоге выяснилось, что реализация
+преподавателя работает быстрее
 """
+import random
+import copy
+import time
+
+list_input = [random.randint(-100, 100) for i in range(10)]
+print(list_input)
+
+
+def time_count(func):
+    def wrapper(*args):
+        start = time.time()
+        res = func(*args)
+        finisf = time.time()
+        print(f'Время выполнения функции {func.__doc__} = {finisf - start}.с')
+        return res
+
+    return wrapper
+
+
+@time_count
+def my_bubble_sort(input_list):
+    """
+    Сортировка пузырёк реализованная мной
+    """
+    i = 1
+    count = 0
+    temp_list = input_list.copy()
+    while count < len(input_list):
+        count += 1
+        if i >= len(input_list):
+            if temp_list == input_list:
+                break
+            else:
+                temp_list = input_list.copy()
+        for i in range(1, len(input_list)):
+            if input_list[-i] > input_list[-i - 1]:
+                input_list[-i], input_list[-i - 1] = input_list[-i - 1], input_list[-i]
+    return input_list
+
+
+@time_count
+def bubble_sort(orig_list):
+    """
+    Сортировка пузырёк реализованная преподавателем
+    """
+    n = 1
+    while n < len(orig_list):
+        for i in range(len(orig_list) - n):
+            if orig_list[i] > orig_list[i + 1]:
+                orig_list[i], orig_list[i + 1] = orig_list[i + 1], orig_list[i]
+        n += 1
+    return orig_list
+
+
+print(my_bubble_sort(list_input))
+print(bubble_sort(list_input))
--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -2,4 +2,50 @@
 2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
+К моему сожалению, я не успел написать свою функцию для алгоритма слияния, однако постарался разобрать предоставленную
+на уроке
 """
+
+import timeit
+import random
+orig_list = [random.randint(0, 50) for i in range(10)]
+print(orig_list)
+def merge_sort(orig_list):
+    if len(orig_list) > 1:
+        center = len(orig_list) // 2
+        left = orig_list[:center]
+        right = orig_list[center:]
+        print(f'{left}   {right}')
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                orig_list[k] = left[i]
+                i += 1
+
+            else:
+                orig_list[k] = right[j]
+                j += 1
+            print(f'orig {orig_list}')
+            k += 1
+
+        while i < len(left):
+            orig_list[k] = left[i]
+            i += 1
+            k += 1
+            print(f'orig_end {orig_list}')
+
+        while j < len(right):
+            orig_list[k] = right[j]
+            j += 1
+            k += 1
+        return orig_list
+
+# замеры
+print(merge_sort(orig_list))
+print(timeit.timeit("merge_sort(orig_list)",
+    setup="from __main__ import merge_sort, orig_list", number=10))
